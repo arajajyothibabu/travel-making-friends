fdab5f0cafd0429c544dc6a2ddaab1aab7b63962
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2018-11-23 12:00:59 +0530

    meteor initialized with material ui

---
+++ imports/ui/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Container from "./layout/Container";
+
+class App extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <Container />
+            </div>
+        )
+    }
+
+}
+export default App;
---
+++ imports/ui/layout/Container.js
@@ -0,0 +1,27 @@
+import React, { Component, Fragment } from 'react';
+import Header from "./Header";
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#6a1b9a',
+        },
+        secondary: red,
+    },
+});
+
+export default class Container extends Component {
+
+    render(){
+        return(
+            <Fragment>
+                <MuiThemeProvider theme={theme}>
+                    <Header />
+                </MuiThemeProvider>
+            </Fragment>
+        )
+    }
+
+}
