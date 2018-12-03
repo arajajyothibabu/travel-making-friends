a9ab882662f8c34c2553f60c6b941f9b6c255b57
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2018-12-03 10:59:11 +0530

    wip: header and hero styling

--- imports/ui/layout/Header.js
+++ imports/ui/layout/Header.js
@@ -8,10 +8,8 @@ import InputBase from '@material-ui/core/InputBase';
 import Badge from '@material-ui/core/Badge';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
-import { fade } from '@material-ui/core/styles/colorManipulator';
 import { withStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MessageIcon from '@material-ui/icons/Message';
 import NotificationsIcon from '@material-ui/icons/Notifications';
@@ -34,45 +32,6 @@ const styles = theme => ({
             display: 'block',
         },
     },
-    search: {
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        marginRight: theme.spacing.unit * 2,
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing.unit * 3,
-            width: 'auto',
-        },
-    },
-    searchIcon: {
-        width: theme.spacing.unit * 6,
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputRoot: {
-        color: 'inherit',
-        width: '100%',
-    },
-    inputInput: {
-        paddingTop: theme.spacing.unit,
-        paddingRight: theme.spacing.unit,
-        paddingBottom: theme.spacing.unit,
-        paddingLeft: theme.spacing.unit * 6,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: 200,
-        },
-    },
     sectionDesktop: {
         display: 'none',
         [theme.breakpoints.up('md')]: {
@@ -86,7 +45,8 @@ const styles = theme => ({
         },
     },
     appBar: {
-        background: 'transparent'
+        background: 'transparent',
+        position: 'absolute'
     }
 });
 
@@ -167,7 +127,7 @@ class Header extends React.Component {
 
         return (
             <div className={classes.root}>
-                <AppBar position="sticky" color="primary">
+                <AppBar position="sticky" color="primary" className={classes.appBar}>
                     <Toolbar variant="dense">
                         {/*<IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
                             <MenuIcon />
@@ -175,18 +135,6 @@ class Header extends React.Component {
                         <Typography className={classes.title} variant="h6" color="inherit" noWrap>
                             Travel Making Friends
                         </Typography>
-                        <div className={classes.search}>
-                            <div className={classes.searchIcon}>
-                                <SearchIcon />
-                            </div>
-                            <InputBase
-                                placeholder="Search…"
-                                classes={{
-                                    root: classes.inputRoot,
-                                    input: classes.inputInput,
-                                }}
-                            />
-                        </div>
                         <div className={classes.grow} />
                         <div className={classes.sectionDesktop}>
                             <IconButton color="inherit">
--- imports/ui/layout/Hero.js
+++ imports/ui/layout/Hero.js
@@ -1,19 +1,119 @@
 import React, { Component, Fragment } from 'react';
-import Header from "./Header";
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import {IMAGES_FOLDER} from "../../../utils";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+import IconButton from "@material-ui/core/IconButton";
+import {fade} from "@material-ui/core/styles/colorManipulator";
 
-export default class Hero extends Component {
+const styles = theme => ({
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.35),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        /*marginRight: theme.spacing.unit * 2,
+        marginLeft: 0,*/
+        top: 80,
+        width: '80%',
+        margin: '0 auto',
+        [theme.breakpoints.up('md')]: {
+            //marginLeft: theme.spacing.unit * 3,
+            width: '40%',
+            top: 130
+        },
+        display: 'flex',
+        color: '#FFFFFF'
+    },
+    searchIcon: {
+        [theme.breakpoints.up('md')]: {
+            width: theme.spacing.unit * 6,
+            padding: theme.spacing.unit * 1.5
+        },
+        width: theme.spacing.unit * 4,
+        height: '100%',
+        padding: theme.spacing.unit,
+        //position: 'absolute',
+        //pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        cursor: 'pointer',
+        color: '#FFFFFF'
+    },
+    inputRoot: {
+        color: 'inherit',
+        width: '100%',
+    },
+    inputInput: {
+        padding: theme.spacing.unit,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            //width: 200,
+            fontSize: 24,
+        },
+        margin: 0,
+        fontSize: 18,
+    },
+    paper: {
+        width: '100%',
+        minHeight: 160,
+        height: '30%',
+        background: `url(${IMAGES_FOLDER}beachview.jpg) no-repeat 100%`,
+        backgroundSize: 'cover',
+        [theme.breakpoints.up('md')]: {
+            //marginLeft: theme.spacing.unit * 3,
+            minHeight: 320,
+        },
+    }
+});
+
+class Hero extends Component {
+
+    state = {
+        q: ''
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        //TODO:
+        console.log(this.state.q);
+    };
+
+    handleQuery = e => {
+        this.setState({q: e.target.value});
+    };
 
     render(){
+        const { classes } = this.props;
         return(
             <Fragment>
-                <Paper style={{width: '100%', minHeight: 320, height: '30%', background: `url(${IMAGES_FOLDER}beachview.jpg) no-repeat 100% 50%`}}>
-                    Araja Jyothi Babu
+                <Paper className={classes.paper}>
+                    <form onSubmit={this.handleSubmit}>
+                        <div className={classes.search}>
+                            <InputBase
+                                placeholder="Search…"
+                                classes={{
+                                    root: classes.inputRoot,
+                                    input: classes.inputInput,
+                                }}
+                                onChange={this.handleQuery}
+                                value={this.state.q}
+                            />
+                            <IconButton type="submit" className={classes.searchIcon}>
+                                <SearchIcon />
+                            </IconButton>
+                        </div>
+                    </form>
                 </Paper>
             </Fragment>
         )
     }
 
 }
+
+export default withStyles(styles)(Hero);
