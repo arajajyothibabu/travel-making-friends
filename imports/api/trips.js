7dcd16da8c2c03aa05a03c816fe90218056df1e3
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2018-12-11 23:09:33 +0530

    trips api

--- imports/api/trips.js
+++ imports/api/trips.js
@@ -1,3 +1,34 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
 
 export const Trips = new Mongo.Collection('tasks');
+
+Meteor.methods({
+    'trips.insert'(text) {
+        check(text, String);
+
+        // Make sure the user is logged in before inserting a trip
+        if (! this.userId) {
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Trips.insert({
+            text,
+            createdAt: new Date(),
+            owner: this.userId,
+            username: Meteor.users.findOne(this.userId).username,
+        });
+    },
+    'trips.remove'(tripId) {
+        check(tripId, String);
+
+        Trips.remove(tripId);
+    },
+    'trips.setChecked'(tripId, setChecked) {
+        check(tripId, String);
+        check(setChecked, Boolean);
+
+        Trips.update(tripId, { $set: { checked: setChecked } });
+    },
+});
