6b50f5c0b15505864657d96ed8b4162ca403aaf0
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2018-12-14 21:57:07 +0530

    transport as option to create a trip

---
+++ imports/constants/index.js
@@ -0,0 +1,7 @@
+export const VEHICLES = {
+    TBD: 'TBD',
+    Bike: 'Bike',
+    Car: 'Car',
+    Bus: 'Bus',
+    //TODO: will add more
+};
--- imports/ui/reusable/NewTripForm.js
+++ imports/ui/reusable/NewTripForm.js
@@ -15,6 +15,13 @@ import Badge from "@material-ui/core/Badge";
 import Hidden from "@material-ui/core/Hidden";
 import LocationCity from '@material-ui/icons/LocationCity';
 import DateRangeTwoTone from '@material-ui/icons/DateRangeTwoTone';
+import {VEHICLES} from "../../constants";
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+import Motorcycle from '@material-ui/icons/Motorcycle';
+import DirectionsCar from '@material-ui/icons/DirectionsCar';
+import DirectionsBus from '@material-ui/icons/DirectionsBus';
+import Commute from '@material-ui/icons/Commute';
 
 const styles = theme => ({
     paper: {
@@ -38,8 +45,24 @@ const styles = theme => ({
         fontSize: theme.spacing.unit * 12,
         display: 'block',
         margin: '0 auto'
-    }
+    },
+    toggleContainer: {
+        height: 56,
+        //padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        margin: `${theme.spacing.unit}px 0`,
+        //background: theme.palette.background.default,
+    },
 });
+
+const VEHICLES_SRC = [
+    { vehicle: VEHICLES.TBD, icon: <Commute/> },
+    { vehicle: VEHICLES.Bike, icon: <Motorcycle/> },
+    { vehicle: VEHICLES.Car, icon: <DirectionsCar/> },
+    { vehicle: VEHICLES.Bus, icon: <DirectionsBus/> }
+];
 
 class NewTripForm extends Component {
 
@@ -51,7 +74,8 @@ class NewTripForm extends Component {
             gang: [],
             notes: '',
             time: moment(),
-            max_size: 1
+            max_size: 1,
+            vehicle: VEHICLES.TBD
         };
     }
 
@@ -72,13 +96,17 @@ class NewTripForm extends Component {
     };
 
     handleSize = (event, max_size) => {
-        this.setState({max_size: max_size > 1 ? max_size : 2});
+        this.setState({max_size: max_size > 0 ? max_size : 1});
+    };
+
+    handleFormat = (event, vehicle) => {
+        this.setState({ vehicle });
     };
 
 
     render(){
         const { classes } = this.props;
-        const { time, max_size, notes } = this.state;
+        const { time, max_size, notes, vehicle } = this.state;
         return(
             <Paper className={classes.paper}>
                 <Grid container spacing={16}>
@@ -99,21 +127,10 @@ class NewTripForm extends Component {
                     <Grid xs={12} md={6} item>
                         <DateTimePicker label="Tentative Date & Time" dateTime={time} handleChange={this.handleTime}/>
                     </Grid>
-                    <Grid xs={12} item>
-                        <TextField
-                            label="Notes"
-                            multiline
-                            rowsMax="20"
-                            value={notes}
-                            onChange={this.handleNotes}
-                            margin="normal"
-                            fullWidth
-                        />
-                    </Grid>
                     <Grid item xs={12}>
                         <Grid container>
                             <Grid item xs={12} sm={4}>
-                                <Typography variant="h6" className={classes.text}>How many can join you ? </Typography>
+                                <Typography variant="subtitle1" className={classes.text}>How many can join you ? </Typography>
                             </Grid>
                             <Grid item xs={12} sm>
                                 <Slider
@@ -136,6 +153,38 @@ class NewTripForm extends Component {
                                 />
                             </Grid>
                         </Grid>
+                    </Grid>
+                    <Grid xs={12} item>
+                        <Grid container>
+                            <Grid item xs={12} sm={4}>
+                                <div className={classes.toggleContainer}>
+                                    <Typography variant="subtitle1" className={classes.text}>Means of transport ?</Typography>
+                                </div>
+                            </Grid>
+                            <Grid item xs={12} sm>
+                                <div className={classes.toggleContainer}>
+                                    <ToggleButtonGroup value={vehicle} exclusive onChange={this.handleFormat}>
+                                        { VEHICLES_SRC.map(({vehicle, icon}) =>
+                                            <ToggleButton value={vehicle} title={vehicle}>
+                                                {icon}
+                                            </ToggleButton>
+                                        )
+                                        }
+                                    </ToggleButtonGroup>
+                                </div>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid xs={12} item>
+                        <TextField
+                            label="Notes"
+                            multiline
+                            rowsMax="20"
+                            value={notes}
+                            onChange={this.handleNotes}
+                            margin="normal"
+                            fullWidth
+                        />
                     </Grid>
                 </Grid>
             </Paper>
